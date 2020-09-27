203573a12e0edd0fefd8a23b87ac0b5e0e837c82
YujiSue <yuji.sue@gmail.com>
2020-09-27 14:55:08 +0900

    update

--- Goetia.cpp
+++ Goetia.cpp
@@ -15,7 +15,7 @@ int main(int argc, const char** argv) {
 	std::string op, cmd;
 	std::vector<std::string> args;
 	++argv;
-	sforin(i, 1, argc) {
+	for (auto i = 1; i < argc; ++i) {
 		op = argv[0];
 		if (op[0] == '-') {
 			if (op[1] == '-') {
@@ -71,49 +71,52 @@ int main(int argc, const char** argv) {
 		++argv;
 	}
 
-#ifdef WIN_OS
-	/*
-	int res = 0;
-	std::string home = getenv("USERPROFILE");
-	res = chdir(home.c_str());
-	std::string workspace = home + "\\AppData\\Local\\slib";
-	chdir(home.c_str());
-
-	mkdir(".slib");
-C:\Windowssystem32\WindowsPowerShell\v1.0\powershell.exe
-	powershell (new - object System.Net.WebClient).Downloadfile("https://www.google.com/", "dl.html")
-	execl("wget", "wget",
-		"--output-document ./master.zip",
-		"https://github.com/YujiSue/slib/archive/master.zip");
-	execl("unzip", "unzip", "./*.zip");
-	
-	
-	res = chdir("slib-master");
-	mkdir("build");
-	res = chdir("build");
-	*/
-
-#else
+#ifdef MAC_OS
 	std::string home = getenv("HOME");
 	std::string workspace = home + "/slib";
 	chdir(home.c_str());
 	system("mkdir slib");
 	chdir(workspace.c_str());
-	system("wget --output-document ./master.zip https://github.com/YujiSue/slib/archive/master.zip");
-	system("unzip ./*.zip");
+	system("curl -Lk -o slib.zip https://github.com/YujiSue/slib/archive/master.zip");
+	system("unzip ./slib.zip");
 	workspace += "/slib-master";
 	chdir(workspace.c_str());
 	system("mkdir build");
 	workspace += "/build";
 	chdir(workspace.c_str());
+#elif defined(WIN_OS)
+	std::string home = getenv("USERPROFILE") + std::string("\\AppData\\Local");
+	std::string workspace = home + "\\slib\\slib-master";
+	chdir(home.c_str());
+	system((std::string("bitsadmin /transfer download https://github.com/YujiSue/slib/archive/master.zip ") + home + "\\slib.zip").c_str());
+	system((std::string("powershell - command \"Expand-Archive -Force") + home + "\\slib.zip " + home + "\\slib\"").c_str());
+	chdir(workspace.c_str());
+	mkdir("build");
+	workspace += "/build";
+	chdir(workspace.c_str());
+#else
+	std::string home = getenv("HOME");
+	std::string workspace = home + "/slib";
+	chdir(home.c_str());
+	mkdir("slib");
+	chdir(workspace.c_str());
+	system("wget --output-document ./master.zip https://github.com/YujiSue/slib/archive/master.zip");
+	system("unzip ./*.zip");
+	workspace += "/slib-master";
+	chdir(workspace.c_str());
+	mkdir("build");
+	workspace += "/build";
+	chdir(workspace.c_str());
 
-#ifdef MAC_OS
-
-
-#elif defined(UNIX_OS) || defined(LINUX_OS)
-
+#ifdef WIN_OS
 	if (lsobj) {
-		cmd = "gcc" + INCLUDE_PATH + LD_PATH + PTHREAD_LIB_PATH + SHARED_FLAG +
+
+
+
+		cl.exe / EHsc / utf - 8 / I"  \slib\include" / c Goetia.cpp
+			link "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib""kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" Goetia.obj
+
+			cmd = "gcc" + INCLUDE_PATH + LD_PATH + PTHREAD_LIB_PATH + SHARED_FLAG +
 			" -O2 -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE -c";
 		sforeach(LIB_Z_C) cmd += " " + LIB_Z + E_ + ".c";
 		sforeach(LIB_TIFF_C) cmd += " " + LIB_TIFF + E_ + ".c";
@@ -141,7 +144,77 @@ C:\Windowssystem32\WindowsPowerShell\v1.0\powershell.exe
 			sforeach(LIB_PNG_C) cmd += " " + E_ + ".o";
 			sforeach(LIB_JPEG_C) cmd += " " + E_ + ".o";
 			sforeach(LIB_SQLITE_C) cmd += " " + E_ + ".o";
-			sforeach(SLIB_BASIC_CPP) cmd += " "+ E_ + ".o";
+			sforeach(SLIB_BASIC_CPP) cmd += " " + E_ + ".o";
+			sforeach(SLIB_SOBJ_CPP) cmd += " " + E_ + ".o";
+			sforeach(SLIB_SIO_CPP) cmd += " " + E_ + ".o";
+			sforeach(SLIB_SUTIL_CPP) cmd += " " + E_ + ".o";
+			sforeach(SLIB_SNET_CPP) cmd += " " + E_ + ".o";
+			sforeach(SLIB_SMEDIA_CPP) cmd += " " + E_ + ".o";
+			std::cout << cmd << std::endl;
+			system(cmd.c_str());
+			system("rm -r ./*.o");
+		}
+		else {
+			cmd = "gcc -shared -o libshelper.so";
+			sforeach(LIB_Z_C) cmd += " " + E_ + ".o";
+			sforeach(LIB_TIFF_C) cmd += " " + E_ + ".o";
+			cmd += " " + UNIQUE_LIBTIFF_C + ".o";
+			sforeach(LIB_PNG_C) cmd += " " + E_ + ".o";
+			sforeach(LIB_JPEG_C) cmd += " " + E_ + ".o";
+			sforeach(LIB_SQLITE_C) cmd += " " + E_ + ".o";
+			std::cout << cmd << std::endl;
+			system(cmd.c_str());
+			system("rm -r ./*.o");
+			cmd = "g++ " + CPP_VERSION + INCLUDE_PATH + PTHREAD_LIB_PATH +
+				" -O2 -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE -c";
+			sforeach(SLIB_BASIC_CPP) cmd += " " + SLIB_BASIC + E_ + ".cpp";
+			sforeach(SLIB_SOBJ_CPP) cmd += " " + SLIB_SOBJ + E_ + ".cpp";
+			sforeach(SLIB_SIO_CPP) cmd += " " + SLIB_SIO + E_ + ".cpp";
+			sforeach(SLIB_SUTIL_CPP) cmd += " " + SLIB_SUTIL + E_ + ".cpp";
+			sforeach(SLIB_SNET_CPP) cmd += " " + SLIB_SNET + E_ + ".cpp";
+			sforeach(SLIB_SMEDIA_CPP) cmd += " " + SLIB_SMEDIA + E_ + ".cpp";
+			std::cout << cmd << std::endl;
+			system(cmd.c_str());
+			system("ar rcs libsobj.a ./*.o");
+			system("rm -r ./*.o");
+		}
+	}
+
+
+#elif defined(UNIX_OS) || defined(LINUX_OS)
+
+	if (lsobj) {
+		cmd = "gcc" + INCLUDE_PATH + LD_PATH + PTHREAD_LIB_PATH + SHARED_FLAG +
+			" -O2 -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE -c";
+		sforeach(LIB_Z_C) cmd += " " + LIB_Z + E_ + ".c";
+		sforeach(LIB_TIFF_C) cmd += " " + LIB_TIFF + E_ + ".c";
+		cmd += " " + LIB_TIFF + UNIQUE_LIBTIFF_C + ".c";
+		sforeach(LIB_PNG_C) cmd += " " + LIB_PNG + E_ + ".c";
+		sforeach(LIB_JPEG_C) cmd += " " + LIB_JPEG + E_ + ".c";
+		sforeach(LIB_SQLITE_C) cmd += " " + LIB_SQLITE + E_ + ".c";
+		sforeach(CRYPTO_C) cmd += " " + CRYPTO + E_ + ".c";
+		std::cout << cmd << std::endl;
+		system(cmd.c_str());
+		if (shared) {
+			cmd = "g++" + CPP_VERSION + INCLUDE_PATH + PTHREAD_LIB_PATH + SHARED_FLAG +
+				" -O2 -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE -c";
+			sforeach(SLIB_BASIC_CPP) cmd += " " + SLIB_BASIC + E_ + ".cpp";
+			sforeach(SLIB_SOBJ_CPP) cmd += " " + SLIB_SOBJ + E_ + ".cpp";
+			sforeach(SLIB_SIO_CPP) cmd += " " + SLIB_SIO + E_ + ".cpp";
+			sforeach(SLIB_SUTIL_CPP) cmd += " " + SLIB_SUTIL + E_ + ".cpp";
+			sforeach(SLIB_SNET_CPP) cmd += " " + SLIB_SNET + E_ + ".cpp";
+			sforeach(SLIB_SMEDIA_CPP) cmd += " " + SLIB_SMEDIA + E_ + ".cpp";
+			std::cout << cmd << std::endl;
+			system(cmd.c_str());
+			cmd = "g++" + CPP_VERSION + CURL_LIB_PATH + " -shared -o libsobj.so";
+			sforeach(LIB_Z_C) cmd += " " + E_ + ".o";
+			sforeach(LIB_TIFF_C) cmd += " " + E_ + ".o";
+			cmd += " " + UNIQUE_LIBTIFF_C + ".o";
+			sforeach(LIB_PNG_C) cmd += " " + E_ + ".o";
+			sforeach(LIB_JPEG_C) cmd += " " + E_ + ".o";
+			sforeach(LIB_SQLITE_C) cmd += " " + E_ + ".o";
+			sforeach(CRYPTO) cmd += " " + E_ + ".o";
+			sforeach(SLIB_BASIC_CPP) cmd += " " + E_ + ".o";
 			sforeach(SLIB_SOBJ_CPP) cmd += " " + E_ + ".o";
 			sforeach(SLIB_SIO_CPP) cmd += " " + E_ + ".o";
 			sforeach(SLIB_SUTIL_CPP) cmd += " " + E_ + ".o";
@@ -268,6 +341,7 @@ C:\Windowssystem32\WindowsPowerShell\v1.0\powershell.exe
 		system("sudo cp -fr ../include/sqlite /usr/local/include/slib");
 		system("sudo cp -fr ../include/sutil /usr/local/include/slib");
 		system("sudo cp -fr ../include/zlib /usr/local/include/slib");
+		system("sudo cp -fr ../include/crypto /usr/local/include/slib");
 	}
 	if (lsapp) {
 		system("sudo cp -fr ../include/sapp /usr/local/include/slib");
--- Goetia.h
+++ Goetia.h
@@ -1,7 +1,120 @@
 #ifndef GOETIA_H
 #define GOETIA_H
 
-#include "sconfig.h"
+
+extern "C" {
+#include <float.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+}
+#include <algorithm>
+#include <chrono>
+#include <ctime>
+#include <cstdio>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <queue>
+#include <random>
+#include <regex>
+#include <utility>
+#include <vector>
+#ifndef _MANAGED
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+#endif
+
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#if TARGET_OS_MAC
+#define MAC_OS
+#include <dirent.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <cmath>
+#endif
+#elif _WIN64
+#define WIN_OS
+#define _USE_MATH_DEFINES
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <direct.h>
+#include <io.h>
+#include <math.h>
+#include <sys/timeb.h>
+#include <windows.h>
+#define execv _execv
+#define popen _popen
+#define pclose _pclose
+#define chdir _chdir
+#define mkdir _mkdir
+#define rmdir _rmdir
+#define S_ISDIR(X) ((X&0170000)==0040000)
+#define S_IRWXU 0x0700
+#define S_IRWXG 0x0070
+#define S_IRWXO 0x0007
+#pragma warning(disable:4996)
+#elif _WIN32
+#define WIN_OS
+#define _USE_MATH_DEFINES
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <direct.h>
+#include <io.h>
+#include <math.h>
+#include <sys/timeb.h>
+#include <windows.h>
+#define execv _execv
+#define popen _popen
+#define pclose _pclose
+#define chdir _chdir
+#define mkdir _mkdir
+#define rmdir _rmdir
+#define S_ISDIR(X) ((X&0170000)==0040000)
+#define S_IRWXU 0x0700
+#define S_IRWXG 0x0070
+#define S_IRWXO 0x0007
+#pragma warning(disable:4996)
+#elif __linux__
+#define OS_TYPE LINUX_OS
+#define LINUX_OS
+#include <dirent.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <cmath>
+#elif __unix__
+#define UNIX_OS
+#include <dirent.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <cmath>
+#endif
 
 extern std::vector<std::string> LIB_JPEG_C = {
 	"jcapimin","jcapistd","jccoefct","jccolor","jcdctmgr","jchuff","jcinit",
@@ -31,6 +144,12 @@ extern std::vector<std::string> LIB_SQLITE_C = {
 extern std::vector<std::string> LIB_Z_C = {
 	"adler32","compress","crc32","deflate","gzclose","gzlib","gzread",
 	"gzwrite","infback","inffast","inflate","inftrees","trees","uncompr", "zutil"
+};
+extern std::vector<std::string> LIB_CURL_C = {
+	
+};
+extern std::vector<std::string> CRYPTO_C = {
+	"md5"
 };
 extern std::vector<std::string> SLIB_BASIC_CPP = {
 	"char","exception","string","sutf8","time"
@@ -88,11 +207,14 @@ extern std::string PTHREAD_LIB_PATH = " -lpthread";
 extern std::string CURL_LIB_PATH = " -lcurl";
 extern std::string SHARED_FLAG = " -fPIC";
 
+extern std::string LIB_CRYPTO = "../src/3rd/crypto/";
+extern std::string LIB_CURL = "../src/3rd/curl/";
 extern std::string LIB_JPEG = "../src/3rd/libjpeg/";
 extern std::string LIB_PNG = "../src/3rd/libpng/";
 extern std::string LIB_TIFF = "../src/3rd/libtiff/";
 extern std::string LIB_SQLITE = "../src/3rd/sqlite/";
 extern std::string LIB_Z = "../src/3rd/zlib/";
+extern std::string CRYPTO = "../src/3rd/crypto/";
 
 extern std::string UNIQUE_LIBTIFF_C = "tif_unix";
 
